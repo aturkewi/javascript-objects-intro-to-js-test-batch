af2f15a3fae8299ce596252a3be1d79cfe0ed578
Avidor Turkewitz <avi.turkewitz@flatironschool.com>
2016-07-10 18:12:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  aesopRock: ['Lotta Years'],
+  jayZ: ['Empire State']
+};
+
+var updatePlaylist = function(playlist, newArtist, newSongs){
+  playlist[newArtist] = newSongs
+  return playlist
+}
+
+var removeFromPlaylist = function(playlist, artistName){
+  delete playlist[artistName]
+  return playlist
+}
